1b74b77b4e78830bc532d6dc96caa3b453bc418c
LATIFARYES <latifaryes@gmail.com>
2024-02-28 18:54:41 +0300

    New Flags

--- Flago/ContentView.swift
+++ Flago/ContentView.swift
@@ -7,18 +7,98 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct Guess_the_Flag: View {
+    
+    // .Shuffled means mix the array again
+    @State private var Flags = ["Germany","Estonia","Nigeria","Poland","Russia","Spain","France","UK", "US" ].shuffled()
+    
+    // for the alert
+    @State private var showingScore = false
+    @State private var showingScore2 = false
+    @State private var scoreTitle = ""
+    @State private var scoreTitle2 = ""
+    
+    // so we can randomly show the flags
+    @State var RandomFlags = Int.random(in: 0...3)
+    
+    // to counter the success
+    @State private var Score = 0
+    @State private var Repeat = 0
+    
+    var body: some View{
+        
+        NavigationView{
+            ZStack(alignment: .center){
+                
+                Color.black.ignoresSafeArea()
+                
+                VStack(alignment: .center, spacing: 15){
+                    Text("Guess The Flag Of")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    
+                    Text(Flags[RandomFlags])
+                        .font(.largeTitle)
+                        .foregroundColor(.green)
+                        .fontWeight(.black)
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(Flags[number])
+                                .renderingMode(.original)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                    }
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("Score: \(Score)")
+                            .foregroundColor(.white)
+                            .font(.title.bold())
+                        Text("Repeat: \(Repeat)")
+                            .foregroundColor(.white)
+                            .font(.title.bold())
+                    }
+                }
+            }.alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is \(Score)")
+            }
+            
+            .alert(scoreTitle2, isPresented: $showingScore2) {
+                Button("Repeat", action: askQuestion)
+                Button("Cancel", role: .cancel, action: executeDelete )
+            }
+
+
         }
-        .padding()
+        
     }
+    func flagTapped(_ number: Int) {
+        if number == RandomFlags {
+            scoreTitle = "Correct 🥰"
+            Score += 1
+            showingScore = true
+        } else {
+            scoreTitle2 = "Try again 🥲"
+            Score = 0
+            Repeat += 1
+            showingScore2 = true
+        }
+    }
+    func askQuestion() {
+        Flags.shuffle()
+        RandomFlags = Int.random(in: 0...2)
+    }
+    
+    func executeDelete() {
+            print("deleting…")
+        }
 }
 
 #Preview {
-    ContentView()
+    Guess_the_Flag()
 }
+
--- Flago/FlagoApp.swift
+++ Flago/FlagoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FlagoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Guess_the_Flag()
         }
     }
 }
